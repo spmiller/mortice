a1d8eb2e45ae63bd25a49434a733b1bd632bca9c
Stuart Miller <stuart.miller@alliedtelesis.co.nz>
2020-03-05 15:34:24 +1300

    Dockerfile and script for running node12 + chromium
    Fingers crossed for quoting issues being fixed now

--- buildenv.sh
+++ buildenv.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 [ -z "$1" ] && INTERACTIVE_FLAG="-i"
 NPMRC_FILE=${HOME}/.npmrc
-[[ -f "${NPMRC_FILE}" ]] && NPMRC_MOUNT="--mount type=bind,source=${NPMRC_FILE},target=/tmp/.npmrc"
-docker run --user ${UID}:${GROUPS} --init --rm --cap-add=SYS_ADMIN "${NPMRC_MOUNT}" -v "${PWD}:${PWD}" -w="${PWD}" ${INTERACTIVE_FLAG} -t morticebuildenv "$@"
+[[ -f "${NPMRC_FILE}" ]] && NPMRC_MOUNT="--mount type=bind,source="${NPMRC_FILE}",target=/tmp/.npmrc"
+docker run --user ${UID}:${GROUPS} --init --rm --cap-add=SYS_ADMIN ${NPMRC_MOUNT} -v "${PWD}:${PWD}" -w="${PWD}" ${INTERACTIVE_FLAG} -t morticebuildenv "$@"
