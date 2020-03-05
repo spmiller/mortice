2a4867d8617ca22f2365bf4029f196f676173f0a
Stuart Miller <stuart.miller@alliedtelesis.co.nz>
2020-03-05 15:31:44 +1300

    Dockerfile and script for running node12 + chromium
    Should fix the spaces in filenames issue.

--- buildenv.sh
+++ buildenv.sh
@@ -2,4 +2,4 @@
 [ -z "$1" ] && INTERACTIVE_FLAG="-i"
 NPMRC_FILE=${HOME}/.npmrc
 [[ -f "${NPMRC_FILE}" ]] && NPMRC_MOUNT="--mount type=bind,source=${NPMRC_FILE},target=/tmp/.npmrc"
-docker run --user ${UID}:${GROUPS} --init --rm --cap-add=SYS_ADMIN ${NPMRC_MOUNT}  -v ${PWD}:${PWD} -w=${PWD} ${INTERACTIVE_FLAG} -t morticebuildenv "$@"
+docker run --user ${UID}:${GROUPS} --init --rm --cap-add=SYS_ADMIN "${NPMRC_MOUNT}" -v "${PWD}:${PWD}" -w="${PWD}" ${INTERACTIVE_FLAG} -t morticebuildenv "$@"
