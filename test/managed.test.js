fd2eca221b50b54e421416d3f516bdf3e5ea0938
Stuart Miller <stuart.miller@alliedtelesis.co.nz>
2020-03-18 20:10:17 +1300

    Managed lock and test

--- test/managed.test.js
+++ test/managed.test.js
@@ -1,5 +1,4 @@
 import test from 'ava'
-import delay from 'delay'
 
 const mortice = require('../')
 
@@ -30,36 +29,3 @@ test('should manage returned write mutexes', async (t) => {
 
   t.true(mutexAtFirstAccess !== mutexAtSecondAccess)
 })
-
-test('should manage multiple mutexes of same name', async (t) => {
-  const name = "some lock"
-
-  const indexes = Array.from({length: 100000})
-
-  const mutexes = await Promise.all(indexes.map(() => new Promise(async resolve => {
-    await delay(2000 + Math.random())
-    const release = await mortice.managed.writeLock(name);
-    const mutex = mortice(name);
-    await release();
-    resolve(mutex)
-  })))
-
-  const mutextDict = mutexes.map(m => m.id).reduce((dict, id) => {
-    dict[id] = (dict[id] || 0) + 1;
-    return dict;
-  }, {})
-
-  t.true(Object.keys(mutextDict).length > 1)
-})
-
-test('should manage multiple mutexes of different names', async (t) => {
-  const indexes = Array.from({length: 100}, (v, i) => i).map(i => "lock" + i)
-
-  await Promise.all(indexes.map(async name => {
-    const release = await mortice.managed.readLock(name);
-    await release();
-  }))
-
-  t.pass("No deadlocks occurred")
-})
-
